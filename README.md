6357bac4fac07e55889ea17f9b6166406239d4e2
craig <ccor5588@gmail.com>
2017-09-20 01:09:17 -0500

    Use bluebird promisify to make callback functions into async/promise functions

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ This is a minimal example of using `kafka-node` with the `ches/kafka` and `zooke
 
 to only see logs from the example service:
 
-`docker-compose up | grep example_service_1`
+`docker-compose up | grep example_service`
 
 Note: There are some timing issues with `ches/kafka` on first `up`. If errors are thrown on up, cancel the process and `docker-compose up` again.
--- app.js
+++ app.js
@@ -1,50 +1,62 @@
 const kafka = require('kafka-node');
+const Promise = require('bluebird');
 
 const client = new kafka.Client('zookeeper:2181');
-const producer = new kafka.Producer(client);
-
-client.on('ready', () => {
-  console.log('client ready');
-  // topic needs test topic to be here to make sure broker is ready -- weird node client hack
-  client.refreshMetadata(['test'], (err) => {
-    if (err) {
-      console.log(`ERROR REFRESHING METADATA: ${err}`);
-    } else {
-      producer.on('error', err => console.log('PRODUCER ERROR:', err));
-      producer.on('ready', () => console.log('producer ready event fired'));  // not seeing this event fire, so using conditional below
-      if (producer.ready) {
-        console.log('producer ready');
-        start();
-      }
-    }
-  });
-});
-
 client.on('error', (err) => console.log(err));
 
+const producer = new kafka.Producer(client);
+producer.on('error', err => console.log('PRODUCER ERROR:', err));
+producer.on('ready', () => console.log('producer ready event fired'));  // not seeing this event fire, so using producer.ready below
 
-const start = () => {
-  printTopics();
-  console.log('creating topics');
-  producer.createTopics(['pizza_topic', 'icecream_topic'], true, (err, data) => {
-    if (err) {
-      return console.log('ERROR CREATING TOPICS:', err);
-    }
-    console.log('topics created: ', data);
+Promise.promisifyAll(client);
+Promise.promisifyAll(producer);
+
+
+client.on('ready', async () => {
+  console.log('client ready');
+
+  // need to call this with test topic to make sure broker is ready -- weird kafka-node client hack
+  await client.refreshMetadataAsync(['test'])
+    .catch(err => console.log(`ERROR REFRESHING METADATA: ${err}`));
+
+  if (producer.ready) {
+    console.log('producer ready');
+    await initTopics();
     initConsumers();
-    sendMessages();
-  });
+    await sendMessages();
+  }
+});
+
+
+const listTopics = async () => {
+  const results = await client.loadMetadataForTopicsAsync([]);
+  return Object.keys(results[1].metadata);
+};
+
+
+const initTopics = async () => {
+  const topics = await listTopics()
+    .catch(err => console.log('ERROR LISTING TOPICS', err));
+
+  console.log('current topics: ', topics);
+
+  if (!['pizza_topic', 'icecream_topic'].every(topic => topics.includes(topic))) {
+    console.log('creating new topics');
+    const data = await producer.createTopicsAsync(['pizza_topic', 'icecream_topic'], true)
+      .catch(err => console.log('ERROR CREATING TOPICS:', err));
+    console.log('topics created: ', data);
+  } else console.log('desired topics already present');
+
 };
 
 
 const initConsumers = () => {
-  console.log('creating consumers');
 
   const consumerClientOne = new kafka.Client('zookeeper:2181');
   const consumerOne = new kafka.Consumer(consumerClientOne, [{ topic: 'pizza_topic' }]);
 
   consumerOne.on('message', (msg) => {
-    console.log('consumer 1 message:', msg);
+    console.log('consumer 1 received message from topic', msg.topic + ':', '\n  ', msg.value);
   });
 
   consumerOne.on('error', (err) => {
@@ -54,28 +66,29 @@ const initConsumers = () => {
   // its recommended to have one client per consumer
   const consumerClientTwo = new kafka.Client('zookeeper:2181');
   const consumerTwo = new kafka.Consumer(
-    consumerClientTwo,  // client 
+    consumerClientTwo,
     [{ topic: 'pizza_topic' }, { topic: 'icecream_topic' }]  // fetch requests (payloads) -- this consumer subscribes to both topics
   );
 
   consumerTwo.on('message', (msg) => {
-    console.log('consumer 2 message', msg);
+    console.log('consumer 2 received message from topic', msg.topic + ':', '\n  ', msg.value);
   });
 
   consumerTwo.on('error', (err) => {
     console.log('ERR IN CONSUMER 2:', err);
   });
 
+  console.log('consumers created');
 };
 
 
-const sendMessages = () => {
+const sendMessages = async () => {
   console.log('producer sending messages');
   const payloads = [
     {
       topic: 'pizza_topic',
       messages: 'message about pizza',
-      attributes: 1  // compression using gzip
+      attributes: 1  // compression using gzip, 2 uses snappy
     },
     {
       topic: 'icecream_topic',
@@ -84,22 +97,8 @@ const sendMessages = () => {
     }
   ];
 
-  producer.send(payloads, (err, data) => {
-    if (err) {
-      console.log(`ERR SENDING PRODUCER MESSAGE: ${err}`)
-    } else {
-      console.log('sent producer message. Info:', data);
-    }
-  });
-};
+  const info = await producer.sendAsync(payloads)
+    .catch(err => console.log(`ERR SENDING PRODUCER MESSAGE: ${err}`));
 
-
-const printTopics = () => {
-  client.loadMetadataForTopics([], function (err, results) {
-    if (err) {
-      return console.log('ERROR LISTING TOPICS:', err);
-    }
-    const topics = Object.keys(results[1].metadata);
-    console.log('topics:', topics);
-  });
-};
+  console.log('producer sent messages, returned info:', info);
+}
